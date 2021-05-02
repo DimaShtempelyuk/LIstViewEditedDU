6d173cd2374434ef60b6abf7b63b7a1dc40b776b
Dima Shtempelyuk <dimon.shtempelyuk@gmail.com>
2021-05-03 01:27:52 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+
+namespace LIstViewEditedDU
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+        private string _surname;
+        public string Surname
+        {
+            get => _surname;
+            set
+            {
+                _surname = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Surname"));
+            }
+        }
+        
+        private DateTime _dob = new DateTime(2000, 01, 01);
+
+        public DateTime Dob
+        {
+            get => _dob;
+            set
+            {
+                _dob = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Dob"));
+            }
+        }
+        public static ObservableCollection<Person> AllPersons { get; set; } = new ObservableCollection<Person>();
+        public static void InitPersons()
+        {
+            AllPersons.Add(new Person
+            {
+                Name = "Andrej",
+                Surname = "Zlatý",
+                Dob = new DateTime(2001, 05, 06)
+
+            });
+            AllPersons.Add(new Person
+            {
+                Name = "Nikita",
+                Surname = "Blatný",
+                Dob = new DateTime(1998, 05, 13)
+            });
+            AllPersons.Add(new Person
+            {
+                Name = "Jan",
+                Surname = "Novák",
+                Dob = new DateTime(2002, 03, 04)
+            });
+        }
+    }
+}
---
+++ PersonEdit.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LIstViewEditedDU
+{
+    /// <summary>
+    /// Interaction logic for PersonEdit.xaml
+    /// </summary>
+    public partial class PersonEdit : Window
+    {
+        bool IsUpdating { get; set; }
+
+        public PersonEdit()
+        {
+            InitializeComponent();
+            DataContext = new Person();
+            IsUpdating = false;
+        }
+
+        public PersonEdit(Person p)
+        {
+            InitializeComponent();
+            DataContext = p;
+            IsUpdating = true;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsUpdating ==false)
+            {
+                Person.AllPersons.Add((Person)DataContext);
+            }
+            this.Close();
+        }
+    }
+}
